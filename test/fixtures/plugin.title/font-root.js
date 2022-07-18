455f4149c1002ab5142597fa96b370ab9a94f359
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-07-18 00:44:19 -0500

    Add unit test for plugin.test root font fallback

---
+++ test/fixtures/plugin.title/font-root.js
@@ -0,0 +1,46 @@
+const data = [];
+for (let x = 0; x < 3; x++) {
+  for (let y = 0; y < 3; y++) {
+    data.push({x, y});
+  }
+}
+
+module.exports = {
+  config: {
+    type: 'scatter',
+    data: {
+      datasets: [{
+        data,
+        backgroundColor: 'red',
+        radius: 1,
+        hoverRadius: 0
+      }],
+    },
+    options: {
+      font: {
+        size: 20,
+      },
+      scales: {
+        x: {display: false},
+        y: {display: false}
+      },
+      plugins: {
+        legend: false,
+        title: {
+          display: true,
+          text: () => 'Title Text',
+        },
+        filler: false,
+        tooltip: false
+      },
+    },
+
+  },
+  options: {
+    spriteText: true,
+    canvas: {
+      height: 400,
+      width: 400
+    }
+  }
+};
